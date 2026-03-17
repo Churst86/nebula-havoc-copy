d91bac57f7b1418708ab2e523509f7451f087731
Xander Orion <xanderorion2016@gmail.com>
2026-03-17 12:58:39 +0000

    File changes

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react';
-import GameCanvas from '../components/game/GameCanvas';
+import GameCanvas from '../components/game/GameCanvas.jsx';
 import GameHUD from '../components/game/GameHUD';
 import GameOver from '../components/game/GameOver';
 import StartScreen from '../components/game/StartScreen';
