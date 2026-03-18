9e6ffd82bf2f7391cd1d8c692864c33c0830de05
Xander Orion <xanderorion2016@gmail.com>
2026-03-18 10:13:45 +0000

    Manual code change

--- src/components/game/GameCanvas.jsx
+++ src/components/game/GameCanvas.jsx
@@ -3,6 +3,7 @@ import MobileControls from './MobileControls';
 import { sounds } from '../../hooks/useSound.js';
 import { spawnBerserk, spawnEater } from '../../lib/enemySpawners.js';
 import { updateBerserkMovement, updateBerserkLaser, drawBerserk } from '../../lib/berserkUtils.js';
+import { fireReverseShot, drawReverseFlame } from '../../lib/reverseGunUtils.js';
 import { DROPPER_COLORS, DROPPER_LABELS, DROPPER_ROTATION } from '../../lib/powerupConfig.js';
 
 // Laser beam constants
