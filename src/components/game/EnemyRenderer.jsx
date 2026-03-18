af9fd675b15e3406a53bdcbc1bed140372bb7f9f
Xander Orion <xanderorion2016@gmail.com>
2026-03-18 12:06:55 +0000

    File changes

--- src/components/game/EnemyRenderer.jsx
+++ src/components/game/EnemyRenderer.jsx
@@ -18,14 +18,20 @@ export function drawEnemy(ctx, e) {
   } else if (e.type === 'elite') {
     ctx.translate(e.x, e.y);
     ctx.shadowColor = '#ffff00';
-    ctx.shadowBlur = 12;
-    ctx.strokeStyle = '#ffff00';
-    ctx.lineWidth = 2;
+    ctx.shadowBlur = 14;
+    ctx.fillStyle = '#ffff00';
     ctx.beginPath();
-    ctx.arc(0, 0, e.w / 2, 0, Math.PI * 2);
-    ctx.stroke();
-    ctx.fillStyle = '#ffff0044';
+    ctx.moveTo(0, -e.h / 2);
+    ctx.lineTo(e.w / 2, -e.h / 4);
+    ctx.lineTo(e.w / 3, e.h / 2);
+    ctx.lineTo(0, e.h / 3);
+    ctx.lineTo(-e.w / 3, e.h / 2);
+    ctx.lineTo(-e.w / 2, -e.h / 4);
+    ctx.closePath();
     ctx.fill();
+    ctx.strokeStyle = '#ffffff';
+    ctx.lineWidth = 1;
+    ctx.stroke();
   } else if (e.type === 'basic') {
     ctx.translate(e.x, e.y);
     ctx.shadowColor = '#44ff88';
