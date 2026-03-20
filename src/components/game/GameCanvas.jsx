4218e0c6171b30b4512a6840e63d80ca197c3418
Xander Orion <xanderorion2016@gmail.com>
2026-03-20 03:21:07 +0000

    File changes

--- src/components/game/GameCanvas.jsx
+++ src/components/game/GameCanvas.jsx
@@ -81,6 +81,7 @@ function initState() {
     dropperRotateTimer: DROPPER_ROTATE_FRAMES, // countdown to rotate dropper type
     starDropperTimer: STAR_SPAWN_INTERVAL,     // separate timer for rare star dropper
     reverseFireTimer: 0,
+    bossWarning: null,
   };
 }
 
