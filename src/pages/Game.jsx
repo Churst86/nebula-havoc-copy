3393bc6e82e66eb4ab7bb6f6c68a3df0c78c8077
Xander Orion <xanderorion2016@gmail.com>
2026-03-20 04:30:06 +0000

    File changes

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useRef, useEffect } from 'react';
-import GameCanvas from '../components/game/GameCanvas';
+import GameCanvas from '../components/game/GameCanvas.jsx';
 import BossWarning from '../components/game/BossWarning.jsx';
 import GameHUD from '../components/game/GameHUD';
 import HighScores, { isHighScore } from '../components/game/HighScores';
