04b1bfe898ac9045a980d33cb83162cf9fc50dab
Xander Orion <xanderorion2016@gmail.com>
2026-03-23 14:42:53 +0000

    File changes

--- src/components/game/GameCanvas.jsx
+++ src/components/game/GameCanvas.jsx
@@ -8,7 +8,7 @@ import { fireMissiles, updateMissiles, drawMissile, getMissileHitDamage, shouldS
 import { DROPPER_COLORS, DROPPER_LABELS, DROPPER_ROTATION } from '../../lib/powerupConfig.js';
 import { drawBlock, drawPiledCells, drawParticle } from '../../lib/drawingUtils.js';
 import { loadSprites, getSprite, getBossSpriteKey, BOSS_SPRITE_MAP, drawSprite } from '../../lib/spriteLoader.js';
-import { useMotionControls } from './useMotionControls.js';
+import { useMotionControls } from './useMotionControls.jsx';
 import {
   createBossWarning, spawnBoss,
   updateBossMovement, updateBossTier1Fire, updateBossTier2Fire,
